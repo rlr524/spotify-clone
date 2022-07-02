b3120a16dee550ed0a5421c17d66bf90d99226f6
Rob Ranf <rob@emiyaconsulting.com>
2022-07-01 18:50:21 -0700

    sidebar links

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,5 +1,16 @@
-import { Box } from "@chakra-ui/layout";
+import {
+  Box,
+  Center,
+  Divider,
+  LinkBox,
+  LinkOverlay,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/layout";
 import NextImage from "next/image";
+import NextLink from "next/link";
+import { navMenu, musicMenu } from "../typescript/navMenu";
 
 const AppSidebar = () => {
   return (
@@ -13,6 +24,47 @@ const AppSidebar = () => {
       <Box paddingY={"20px"}>
         <Box width={"120px"} marginBottom={"20px"} paddingX={"20px"}>
           <NextImage src={"/app_logo.svg"} height={60} width={120} />
+        </Box>
+        <Box marginBottom={"20px"}>
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem key={menu.name} paddingX={"20px"} fontSize={"16px"}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color={"white"}
+                        marginRight={"20px"}
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+            <Divider
+              orientation={"horizontal"}
+              width={"80px"}
+              alignSelf={"center"}
+            />
+            {musicMenu.map((menu) => (
+              <ListItem key={menu.name} paddingX={"20px"} fontSize={"16px"}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color={"white"}
+                        marginRight={"20px"}
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
---
+++ typescript/navMenu.ts
@@ -0,0 +1,40 @@
+import {
+  MdFavorite,
+  MdHome,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdSearch,
+} from "react-icons/md";
+
+const navMenu: Array<any> = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
+
+const musicMenu: Array<any> = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
+
+export { navMenu, musicMenu };
