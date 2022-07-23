0c9c1c76b21663f9a1a57cdd857c5baea4ffe06c
Rob Ranf <rob@emiyaconsulting.com>
2022-07-23 12:26:31 -0700

    more troubleshooting of playlist endpoint

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -63,7 +63,7 @@ const AppSidebar = () => {
     >
       <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
-          <NextImage src="/logo.svg" height={60} width={120} />
+          <NextImage src="/app_logo.svg" height={60} width={120} />
         </Box>
         <Box marginBottom="20px">
           <List spacing={2}>
